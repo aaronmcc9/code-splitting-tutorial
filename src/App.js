45a5845b186c2015566eb911d436f8a42df49411
Aaron McCarthy <Aaron.McCarthy@logibec.com>
2023-04-10 14:41:50 -0700

    quick pages set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Page1 from './components/page1';
+import Page2 from './components/page2';
+import Page3 from './components/page3';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [route, setRoute] = useState('page1');
+
+  const onRouteChange = (route) => {
+    setRoute(route);
+  }
+
+  console.log(route);
+  if (route == "page1")
+    return <Page1 onRouteChange={onRouteChange} />
+
+  if (route == "page2")
+    return <Page2 onRouteChange={onRouteChange} />
+
+  if (route == "page3")
+    return <Page3 onRouteChange={onRouteChange} />
+
 }
 
 export default App;
